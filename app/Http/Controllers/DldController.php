4862e53a0e6a3843746318f4616e172fae1ad4be
fandykun <fandy19061999@gmail.com>
2019-07-07 02:06:55 +0700

    add default auth, model, and controller

---
+++ app/Http/Controllers/DldController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Dld;
+use Illuminate\Http\Request;
+
+class DldController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Dld  $dld
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Dld $dld)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Dld  $dld
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Dld $dld)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Dld  $dld
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Dld $dld)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Dld  $dld
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Dld $dld)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/RegionalManagerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\RegionalManager;
+use Illuminate\Http\Request;
+
+class RegionalManagerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\RegionalManager  $regionalManager
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RegionalManager $regionalManager)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\RegionalManager  $regionalManager
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RegionalManager $regionalManager)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\RegionalManager  $regionalManager
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RegionalManager $regionalManager)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\RegionalManager  $regionalManager
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RegionalManager $regionalManager)
+    {
+        //
+    }
+}
