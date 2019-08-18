f9ae4234204c70d927fa7c4855c83ba84ba670b2
andikaandra <andikaandra03@gmail.com>
2019-08-18 20:16:18 +0700

    update

--- resources/views/partials/contact.blade.php
+++ resources/views/partials/contact.blade.php
@@ -1,11 +1,11 @@
 <section class="bg-gray section-padding">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-12 col-md-6 text-center">
+            <div class="col-12 col-md-6 text-center mb-5">
               <h2 style="display: inline;" class="align-middle text-muted">Ikuti kami lebih jauh</h2>
             </div>
             <div class="col-12 col-md-6 text-center ">
-                 <a style="display: inline;" class="align-middle button button-hero" role="button" href="{{ url('/contact') }}">Temukan Kami&emsp;<i class="fas fa-arrow-circle-right"></i></a>
+                 <a style="display: inline; background-color: #f8b500" class="align-middle button button-hero" role="button" href="{{ url('/contact') }}">Temukan Kami&emsp;<i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
     </div>
